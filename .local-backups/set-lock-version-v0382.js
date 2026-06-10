1f68c9e269e52a5721eb456d1be4626d0054d3ac
Marco Geißhirt <196677227+Randspringer90@users.noreply.github.com>
2026-06-10 14:51:05 +0200

    Remove local audit and backup files from repository

--- .local-backups/set-lock-version-v0382.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = process.argv[1];
-const version = process.argv[2];
-const lock = JSON.parse(fs.readFileSync(path, 'utf8'));
-lock.version = version;
-if (lock.packages && lock.packages['']) {
-  lock.packages[''].version = version;
-}
-fs.writeFileSync(path, JSON.stringify(lock, null, 2) + '\n', 'utf8');
